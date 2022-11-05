d20f5af2584daf62081c0fb898ab1ac2c25c860e
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-11-05 12:17:34 +0000

    sdk: add ping/pong messages

--- sdk/protocol.go
+++ sdk/protocol.go
@@ -7,23 +7,47 @@ import (
 type MessageType uint16
 
 const (
-	TypePublishMessage = MessageType(1)
-	TypeTopicMessage   = MessageType(2)
+	TypeAttach   = MessageType(1)
+	TypeAttached = MessageType(2)
+	TypePublish  = MessageType(3)
+	TypeAck      = MessageType(4)
+	TypeMessage  = MessageType(5)
+	TypePing     = MessageType(6)
+	TypePong     = MessageType(7)
 )
 
-type PublishMessage struct {
-	Message []byte
+type PingMessage struct {
+	// Timestamp is the time in milliseconds the ping message was sent.
+	Timestamp int64
 }
 
-type TopicMessage struct {
-	Offset  uint64
-	Message []byte
+func NewPingMessage(timestamp int64) *ProtocolMessage {
+	return &ProtocolMessage{
+		Type: TypePing,
+		Ping: &PingMessage{
+			Timestamp: timestamp,
+		},
+	}
+}
+
+type PongMessage struct {
+	// Timestamp echos back the timestamp from the corresponding ping message.
+	Timestamp int64
+}
+
+func NewPongMessage(timestamp int64) *ProtocolMessage {
+	return &ProtocolMessage{
+		Type: TypePong,
+		Pong: &PongMessage{
+			Timestamp: timestamp,
+		},
+	}
 }
 
 type ProtocolMessage struct {
-	Type           MessageType
-	PublishMessage *PublishMessage
-	TopicMessage   *TopicMessage
+	Type MessageType
+	Ping *PingMessage
+	Pong *PongMessage
 }
 
 func (m *ProtocolMessage) Encode() ([]byte, error) {
