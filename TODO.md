9cad4c5b051fbd1fd80235b691f50d2373febad2
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-11-05 11:46:34 +0000

    sdk: add system test for sdk reconnect after disconnect

--- TODO.md
+++ TODO.md
@@ -5,5 +5,6 @@
   * Either if the initial connection fails or if the connection drops
   * Testing
     * Test connecting 5 seconds before the server starts
-    * Test reconnecting after the server connection is dropped
+	* Test server responds with RST for 2 seconds and client still reconnects
+	* Test server never responds to ping and SDK reconnects
 * Add heartbeats
--- tests/sdk/connection_test.go
+++ tests/sdk/connection_test.go
@@ -74,3 +74,43 @@ func TestConnection_ConnectOnceReachable(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, wombat.StateConnected, evt)
 }
+
+// Tests if the connection to wombat is disconnected the SDK detects the
+// disconnection and tries to reconnect.
+func TestConnection_ReconnectAfterDisconnected(t *testing.T) {
+	cluster, err := wcm.NewCluster()
+	assert.Nil(t, err)
+	defer cluster.Shutdown()
+
+	node, err := cluster.AddNode()
+	assert.Nil(t, err)
+
+	stateSubscriber := wombat.NewChannelStateSubscriber()
+	logger, _ := zap.NewDevelopment()
+	client, err := wombat.NewWombat(&wombat.Config{
+		Addr:            node.Addr,
+		StateSubscriber: stateSubscriber,
+		Logger:          logger,
+	})
+	assert.Nil(t, err)
+	defer client.Shutdown()
+
+	evt, ok := waitForStateWithTimeout(stateSubscriber, 5*time.Second)
+	assert.True(t, ok)
+	assert.Equal(t, wombat.StateConnected, evt)
+
+	// Disable the networking for the node and reenable after 1 second.
+	assert.Nil(t, node.Disable())
+	go func() {
+		<-time.After(1 * time.Second)
+		assert.Nil(t, node.Enable())
+	}()
+
+	evt, ok = waitForStateWithTimeout(stateSubscriber, 5*time.Second)
+	assert.True(t, ok)
+	assert.Equal(t, wombat.StateDisconnected, evt)
+
+	evt, ok = waitForStateWithTimeout(stateSubscriber, 5*time.Second)
+	assert.True(t, ok)
+	assert.Equal(t, wombat.StateConnected, evt)
+}
