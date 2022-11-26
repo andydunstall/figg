ec5bb3b35197237842a4a2326acc86593939630e
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-11-26 16:12:37 +0000

    sdk: fix wait race

--- sdk/client.go
+++ sdk/client.go
@@ -131,7 +131,11 @@ func (c *Client) writeLoop() {
 
 	for {
 		c.mu.Lock()
-		c.cv.Wait()
+		// Only block if we don't have any outgoing messagese to process
+		// (otherwise we can miss signals and deadlock).
+		if len(c.outgoing) == 0 {
+			c.cv.Wait()
+		}
 		c.mu.Unlock()
 
 		outgoing := c.takeOutgoing()
