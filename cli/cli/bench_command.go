2acca214388105465260d62f9e22d6a08f18426b
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-11-26 13:20:44 +0000

    cli: add samples to bench

--- cli/cli/bench_command.go
+++ cli/cli/bench_command.go
@@ -11,6 +11,7 @@ import (
 type BenchCommand struct {
 	Config   *FiggConfig
 	cobraCmd *cobra.Command
+	samples  int
 }
 
 func NewBenchCommand(config *FiggConfig) *BenchCommand {
@@ -23,6 +24,7 @@ func NewBenchCommand(config *FiggConfig) *BenchCommand {
 			return command.run()
 		},
 	}
+	cobraCmd.PersistentFlags().IntVar(&command.samples, "samples", 1, "number of bench samples")
 	command.cobraCmd = cobraCmd
 	return command
 }
@@ -36,49 +38,50 @@ func (c *BenchCommand) CobraCommand() *cobra.Command {
 }
 
 func (c *BenchCommand) run() error {
-	publisher, err := figg.NewFigg(&figg.Config{
-		Addr: c.Config.Addr,
-	})
-	if err != nil {
-		return err
-	}
-
-	subscriber, err := figg.NewFigg(&figg.Config{
-		Addr: c.Config.Addr,
-	})
-	if err != nil {
-		return err
-	}
-
-	<-time.After(time.Second)
-
-	// Wait to become connected and attached.
-	// TODO(AD) this should be an event (maybe Subscribe should block until
-	// received ATTACHED)
-
-	doneCh := make(chan interface{})
-
-	count := 10000
-	received := 0
-	subscriber.Subscribe("bench-topic", func(topic string, m []byte) {
-		received++
-		if received == count {
-			close(doneCh)
+	for i := 0; i != c.samples; i++ {
+		publisher, err := figg.NewFigg(&figg.Config{
+			Addr: c.Config.Addr,
+		})
+		if err != nil {
+			return err
 		}
-	})
 
-	<-time.After(time.Second)
+		subscriber, err := figg.NewFigg(&figg.Config{
+			Addr: c.Config.Addr,
+		})
+		if err != nil {
+			return err
+		}
 
-	start := time.Now()
+		<-time.After(time.Second)
 
-	for i := 0; i != count; i++ {
-		publisher.Publish("bench-topic", []byte(fmt.Sprintf("message-%d", i)))
+		// Wait to become connected and attached.
+		// TODO(AD) this should be an event (maybe Subscribe should block until
+		// received ATTACHED)
+
+		doneCh := make(chan interface{})
+
+		count := 10000
+		received := 0
+		subscriber.Subscribe("bench-topic", func(topic string, m []byte) {
+			received++
+			if received == count {
+				close(doneCh)
+			}
+		})
+
+		<-time.After(time.Second)
+
+		start := time.Now()
+
+		for i := 0; i != count; i++ {
+			publisher.Publish("bench-topic", []byte(fmt.Sprintf("message-%d", i)))
+		}
+
+		<-doneCh
+
+		elapsed := time.Since(start)
+		fmt.Println("elapsed", elapsed, "count", count)
 	}
-
-	<-doneCh
-
-	elapsed := time.Since(start)
-	fmt.Println("elapsed", elapsed, "count", count)
-
 	return nil
 }
--- cli/cli/figg_command.go
+++ cli/cli/figg_command.go
@@ -1,24 +1,12 @@
 package cli
 
 import (
-	// "fmt"
-	// "os"
-
 	"github.com/spf13/cobra"
 )
 
 var (
 	figgAddr string
 )
-
-// var rootCmd = &cobra.Command{
-// 	Use: "figg",
-// 	Run: func(cmd *cobra.Command, args []string) {},
-// }
-
-// func init() {
-// 	rootCmd.PersistentFlags().StringVar(&figgAddr, "addr", "127.0.0.1:8119", "figg cluster address")
-// }
 
 type FiggConfig struct {
 	Addr string
