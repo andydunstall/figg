9a41f195dde6fe30317ea9928fb8f2e3be7ab098
Andrew Dunstall <andydunstall@hotmail.co.uk>
2022-12-01 06:58:00 +0000

    topic: benchmark wait to receive expected messages

--- service/pkg/topic/subscriptions_test.go
+++ service/pkg/topic/subscriptions_test.go
@@ -7,21 +7,34 @@ import (
 )
 
 type nopAttachment struct {
+	DoneCh   chan interface{}
+	expected int
+	received int
 }
 
-func newNopAttachment() Attachment {
-	return &nopAttachment{}
+func newNopAttachment(expected int) *nopAttachment {
+	return &nopAttachment{
+		DoneCh:   make(chan interface{}),
+		expected: expected,
+		received: 0,
+	}
 }
 
-func (a *nopAttachment) Send(m TopicMessage) {}
+func (a *nopAttachment) Send(m TopicMessage) {
+	a.received++
+	if a.expected == a.received {
+		close(a.DoneCh)
+	}
+}
 
-func benchmarkTopic(topicName string, publishes int, subscribers int, messageLen int) {
+func benchmarkTopicPublish(topicName string, publishes int, subscribers int, messageLen int) {
 	broker := NewBroker()
 
 	message := make([]byte, messageLen)
 	rand.Read(message)
 
-	subscriptions := NewSubscriptions(broker, newNopAttachment())
+	attachment := newNopAttachment(publishes)
+	subscriptions := NewSubscriptions(broker, attachment)
 	for i := 0; i != subscribers; i++ {
 		subscriptions.AddSubscription(topicName)
 	}
@@ -30,46 +43,48 @@ func benchmarkTopic(topicName string, publishes int, subscribers int, messageLen
 	for i := 0; i != publishes; i++ {
 		topic.Publish(message)
 	}
+
+	<-attachment.DoneCh
 }
 
 func BenchmarkTopic_Pub100_Sub1_M10(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 100, 1, 10)
+		benchmarkTopicPublish(topicName, 100, 1, 10)
 	}
 }
 
 func BenchmarkTopic_Pub100_Sub1000_M10(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 100, 1000, 10)
+		benchmarkTopicPublish(topicName, 100, 1000, 10)
 	}
 }
 
 func BenchmarkTopic_Pub1000_Sub1_M10(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 1000, 1, 10)
+		benchmarkTopicPublish(topicName, 1000, 1, 10)
 	}
 }
 
 func BenchmarkTopic_Pub1000_Sub1000_M10(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 1000, 1000, 10)
+		benchmarkTopicPublish(topicName, 1000, 1000, 10)
 	}
 }
 
 func BenchmarkTopic_Pub1000_Sub1_M256KB(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 1000, 1, 256000)
+		benchmarkTopicPublish(topicName, 1000, 1, 256000)
 	}
 }
 
 func BenchmarkTopic_Pub1000_Sub1000_M256KB(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		topicName := fmt.Sprintf("bench-topic-%d", n)
-		benchmarkTopic(topicName, 1000, 1000, 256000)
+		benchmarkTopicPublish(topicName, 1000, 1000, 256000)
 	}
 }
